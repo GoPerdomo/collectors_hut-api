d8c54c7d46068c5c8ede60668213498efbe59b04
Gabriel de O. Perdomo <goperdomo@gmail.com>
2017-11-26 18:42:59 +0100

    Implement fake data for tests

---
+++ tests/data.js
@@ -0,0 +1,32 @@
+const userData = {
+  firstName: "John",
+  lastName: "Userman",
+  photo: "https://d30y9cdsu7xlg0.cloudfront.net/png/17241-200.png",
+  email: "johnuserman@email.com",
+  password: "secret",
+  newFirstName: "Mary",
+  newLastName: "McUser",
+  newPhoto: "https://cdn3.iconfinder.com/data/icons/rcons-user-action/32/girl-512.png",
+  newEmail: "marymcuser@email.com",
+  newPassword: "moresecret",
+};
+
+const collectionData = {
+  name: "Coins",
+  newName: "Stamps",
+};
+
+const itemData = {
+  name: "Cool Item",
+  photo: "https://image.flaticon.com/icons/svg/217/217853.svg",
+  description: "Rare cool item",
+  extraInfo: "Some extra info",
+  newItemName: "Super Cool Item",
+  newItemPhoto: "https://d30y9cdsu7xlg0.cloudfront.net/png/71297-200.png",
+  newItemDescription: "Rare super cool item",
+  newItemExtraInfo: "Some super extra info",
+};
+
+module.exports.userData = userData;
+module.exports.collectionData = collectionData;
+module.exports.itemData = itemData;
--- tests/schemas.test.js
+++ tests/schemas.test.js
@@ -4,20 +4,21 @@ const User = require('../src/models/User');
 const Collection = require('../src/models/Collection');
 const Item = require('../src/models/Item');
 
+// Data for tests
+const userData = require('./data').userData;
+const collectionData = require('./data').collectionData;
+const itemData = require('./data').itemData;
+// Data for tests
+
+
 // User tests
 test('Testing user schema', (t) => {
-  const firstName = "John";
-  const lastName = "Userman";
-  const photo = "https://d30y9cdsu7xlg0.cloudfront.net/png/17241-200.png";
-  const email = "johnuserman@email.com";
-  const password = "secret";
-  
   const newUser = new User({
-    firstName: firstName,
-    lastName: lastName,
-    photo: photo,
-    email: email,
-    password: password,
+    firstName: userData.firstName,
+    lastName: userData.lastName,
+    photo: userData.photo,
+    email: userData.email,
+    password: userData.password,
   });
   const userId = newUser._id;
   
@@ -34,24 +35,18 @@ test('Testing user schema', (t) => {
   });
 
   t.test('when a user is updated', (t) => {
-    const newFirstName = "Mary";
-    const newLastName = "McUser";
-    const newPhoto = "https://cdn3.iconfinder.com/data/icons/rcons-user-action/32/girl-512.png";
-    const newEmail = "marymcuser@email.com";
-    const newPassword = "moresecret";
-
-    newUser.firstName = newFirstName;
-    newUser.lastName = newLastName;
-    newUser.photo = newPhoto;
-    newUser.email = newEmail;
-    newUser.password = newPassword;
+    newUser.firstName = userData.newFirstName;
+    newUser.lastName = userData.newLastName;
+    newUser.photo = userData.newPhoto;
+    newUser.email = userData.newEmail;
+    newUser.password = userData.newPassword;
     newUser.collections.push('new collection');
     
-    t.deepEqual(newUser.firstName, newFirstName, 'first name should match new the one');
-    t.deepEqual(newUser.lastName, newLastName, 'last name should match new the one');
-    t.deepEqual(newUser.photo, newPhoto, 'photo should match new the one');
-    t.deepEqual(newUser.email, newEmail, 'email should match new the one');
-    t.deepEqual(newUser.password, newPassword, 'password should match new the one');
+    t.deepEqual(newUser.firstName, userData.newFirstName, 'first name should match new the one');
+    t.deepEqual(newUser.lastName, userData.newLastName, 'last name should match new the one');
+    t.deepEqual(newUser.photo, userData.newPhoto, 'photo should match new the one');
+    t.deepEqual(newUser.email, userData.newEmail, 'email should match new the one');
+    t.deepEqual(newUser.password, userData.newPassword, 'password should match new the one');
     t.notDeepEqual(newUser.collections.length, 0, 'collections array should not be empty');
     t.end();
   });
@@ -61,10 +56,8 @@ test('Testing user schema', (t) => {
 
 // Collection tests
 test('Testing collection schema', (t) => {
-  const collectionName = "Coins";
-
   const newCollection = new Collection({
-    name: collectionName
+    name: collectionData.name
   });
 
   t.test('when a new collection is created', (t) => {
@@ -76,12 +69,10 @@ test('Testing collection schema', (t) => {
   });
 
   t.test('when a collection is updated', (t) => {
-    const newCollectionName = "Stamps";
-
-    newCollection.name = newCollectionName;
+    newCollection.name = collectionData.newName;
     newCollection.items.push('new item');
 
-    t.deepEqual(newCollection.name, newCollectionName, 'name should match new the one');
+    t.deepEqual(newCollection.name, collectionData.newName, 'name should match new the one');
     t.notDeepEqual(newCollection.items.length, 0, 'items array should not be empty');
     t.end();
   });
@@ -91,16 +82,11 @@ test('Testing collection schema', (t) => {
 
 // Item tests
 test('Testing item schema', (t) => {
-  const itemName = "Cool Item";
-  const itemPhoto = "https://image.flaticon.com/icons/svg/217/217853.svg";
-  const itemDescription = "Rare cool item";
-  const itemExtraInfo = "Some extra info";
-
   const newItem = new Item({
-    name: itemName,
-    photo: itemPhoto,
-    description: itemDescription,
-    extraInfo: itemExtraInfo,
+    name: itemData.name,
+    photo: itemData.photo,
+    description: itemData.description,
+    extraInfo: itemData.extraInfo,
   });
 
   t.test('when a new item is created', (t) => {
@@ -113,20 +99,15 @@ test('Testing item schema', (t) => {
   });
 
   t.test('when a item is updated', (t) => {
-    const newItemName = "Super Cool Item";
-    const newItemPhoto = "https://d30y9cdsu7xlg0.cloudfront.net/png/71297-200.png";
-    const newItemDescription = "Rare super cool item";
-    const newItemExtraInfo = "Some super extra info";
+    newItem.name = itemData.newItemName,
+    newItem.photo = itemData.newItemPhoto,
+    newItem.description = itemData.newItemDescription,
+    newItem.extraInfo = itemData.newItemExtraInfo,
 
-    newItem.name = newItemName,
-    newItem.photo = newItemPhoto,
-    newItem.description = newItemDescription,
-    newItem.extraInfo = newItemExtraInfo,
-
-    t.deepEqual(newItem.name, newItemName, 'name should match new the one');
-    t.deepEqual(newItem.photo, newItemPhoto, 'photo should match new the one');
-    t.deepEqual(newItem.description, newItemDescription, 'description should match new the one');
-    t.deepEqual(newItem.extraInfo, newItemExtraInfo, 'extra info should match new the one');
+    t.deepEqual(newItem.name, itemData.newItemName, 'name should match new the one');
+    t.deepEqual(newItem.photo, itemData.newItemPhoto, 'photo should match new the one');
+    t.deepEqual(newItem.description, itemData.newItemDescription, 'description should match new the one');
+    t.deepEqual(newItem.extraInfo, itemData.newItemExtraInfo, 'extra info should match new the one');
     t.end();
   });
 });
